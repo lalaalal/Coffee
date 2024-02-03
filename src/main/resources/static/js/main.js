8483684ea63695f1d6e984c9733f9c0ab839277c
lalaalal <bontin0128@gmail.com>
2024-02-03 18:36:16 +0900

    menu-add ui

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,10 +8,41 @@ document.addEventListener('DOMContentLoaded', () => {
             request.onreadystatechange = function () {
                 if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                     part.innerHTML = this.responseText;
+                    if (part.id)
+                        init('#' + part.id);
                 }
             };
             request.open('GET', file, true);
             request.send();
         }
     }
+    init();
 });
+
+function init(prefix) {
+    const observers = $(`${prefix} .observer`);
+    observers.change(function() {
+        const inputGroup = $(`input[name=${this.name}]`);
+        for (const input of inputGroup) {
+            let linkedLabel = $(`label[for=${input.id}]`);
+            if (this.id === input.id)
+                linkedLabel.css('background-color', '#eee');
+            else
+                linkedLabel.css('background-color', 'transparent');
+        }
+    });
+    const selected = $(`${prefix} .observer:checked`)[0];
+    if (selected)
+        colorElement(selected);
+}
+
+function colorElement(element) {
+    const inputGroup = $(`input[name=${element.name}]`);
+    for (const input of inputGroup) {
+        let linkedLabel = $(`label[for=${input.id}]`);
+        if (element.id === input.id)
+            linkedLabel.css('background-color', '#eee');
+        else
+            linkedLabel.css('background-color', 'transparent');
+    }
+}
