acbd7ecdd91ae65be35883c66486e75818698d82
lalaalal <bontin0128@gmail.com>
2024-02-08 19:42:07 +0900

    login page

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#login-button").click(function() {
+        let id = $("#id-input")[0].value;
+        let password = $("#password-input")[0].value;
+        let data = `id = ${id} & password = ${password}`;
+        console.log(data);
+        $.ajax({
+            url: "/api/login",
+            type: "POST",
+            contentType: "application/x-www-form-urlencoded",
+            data: `id=${id}&\npassword=${password}`
+        }).done(function(data, status, xhr) {
+            alert(data.message);
+            location.href ='/';
+        }).fail(function(xhr, status, error) {
+            console.log(xhr);
+        });
+    })
+});
